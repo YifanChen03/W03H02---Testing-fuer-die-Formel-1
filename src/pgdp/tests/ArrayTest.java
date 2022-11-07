4f376a52b0a74b3d492a3ab2d0ca0168b05dc8aa
cyanfhine <yifan03.chen@tum.de>
2022-11-07 14:31:26 +0100

    testing function print()

--- src/pgdp/tests/ArrayTest.java
+++ src/pgdp/tests/ArrayTest.java
@@ -1,6 +1,7 @@
 package pgdp.tests;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
 import static pgdp.array.Array.print;
 import static pgdp.array.Array.minAndMax;
 import static pgdp.array.Array.invert;
@@ -8,13 +9,39 @@ import static pgdp.array.Array.intersect;
 import static pgdp.array.Array.linearize;
 import static pgdp.array.Array.bubbleSort;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import org.junit.jupiter.api.Test;
+import pgdp.array.ArrayInterface;
 
 public class ArrayTest {
+	private static final ByteArrayOutputStream out = new ByteArrayOutputStream() {
+		// returns platform independent captured output
+		@Override
+		public synchronized String toString() {
+			return super.toString().replace("\r", "");
+		}
+	};
+	private static final PrintStream stdOut = System.out;
+	@AfterEach
+	public void resetBuffer() {
+		out.reset();
+	}
+
+	@BeforeAll
+	public static void setUpStreams() {
+		System.setOut(new PrintStream(out));
+	}
+
+	@AfterAll
+	public static void restoreStreams() {
+		System.setOut(stdOut);
+	}
 
 	@Test
-	void test() {
-		print(new int[] { 1, 2, 3 });
-		fail();
+	void test_print() {
+		print(new int[]{-12123, 123213, 3, 123, 0, 3, 4, 56, 43, 234, 234, 45, 0, 2});
+		assertEquals("{-12123, 123213, 3, 123, 0, 3, 4, 56, 43, 234, 234, 45, 0, 2}\n", out.toString());
 	}
 }
